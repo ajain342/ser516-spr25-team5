5171492c18e21fc9cdb39ecc9b479a0c064187c9
Jeteish Pratap Singh <jsing162@asu.edu>
2025-02-15 17:08:32 -0700

    added link parser in api contoller  to reslove issue with codetabs

--- LOC-api/app.py
+++ LOC-api/app.py
@@ -1,8 +1,18 @@
 from flask import Flask, request, jsonify
 from modified_LOC import fetch_loc_cloc
 from online_Tool import fetch_loc_codetabs
+from urllib.parse import urlparse
 
 app = Flask(__name__)
+
+def parse_url(repo_url):
+    parsed_url = urlparse(repo_url)
+    repo_path = parsed_url.path.strip("/")
+    if repo_path.endswith(".git"):
+        repo_path = repo_path[:-4]
+    if repo_path.count('/') != 1:
+        return None
+    return repo_path
 
 @app.route('/')
 def home():
@@ -17,12 +27,16 @@ def get_loc():
     
     repo_url = data['repo_url']
     method = data.get('method', 'cloc')  # Default to cloc
-    
+
+    repo_path = parse_url(repo_url)
+    if not repo_path:
+        return jsonify({"error": "Invalid GitHub repository URL"}), 400
+
     try:
         if method == 'cloc':
             result = fetch_loc_cloc(repo_url)
         elif method == 'codetabs':
-            result = fetch_loc_codetabs(repo_url)
+            result = fetch_loc_codetabs(repo_path)
         else:
             return jsonify({"error": "Invalid method. Use 'cloc' or 'codetabs'"}), 400
         
