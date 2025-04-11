b2b3ad03fba1c489a1f4b75701adeb4fbdbbb69a
Piyush Sharma <pshar169@asu.edu>
2025-04-11 15:33:43 -0700

    task 97 cc test

--- modules/CC_api/cc_tests.py
+++ modules/CC_api/cc_tests.py
@@ -1,29 +1,35 @@
 import unittest
 import json
-from app import app 
+import requests
+from flask import jsonify
 
 class TestCodeChurnAPI(unittest.TestCase):
-    def setUp(self):
-        self.client = app.test_client()
+    API_URL = "http://localhost:5001/code-churn"
 
-    def test_success_response(self):
-        response = self.client.post('/code-churn',
-                                    json={"repo_url": "https://github.com/mikaelvesavuori/github-dora-metrics.git",
-                                          "num_commits_before_latest": 2})
+    def test_api_response(self):
+        payload = {
+                    'repo_url': 'https://github.com/kgary/ser421public',
+                    'method': 'online',
+                    'num_commits_before_latest': 10
+                }
+        response = requests.post(self.API_URL, json=payload)
         self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertIn("added_lines", data)
+        self.assertIn("commit_range", data)
+        self.assertIn("deleted_lines", data)
+        self.assertIn("method", data)
+        self.assertIn("modified_lines", data)
+        self.assertIn("result", data)
+        self.assertIn("total_commits", data)
 
-    def test_missing_repo_url(self):
-        response = self.client.post('/code-churn',
-                                    json={"num_commits_before_latest": 2})
-        self.assertEqual(response.status_code, 400)
-        self.assertIn("Missing 'repo_url'", response.get_json().get('error', ''))
-
-    def test_invalid_commit_number(self):
-        response = self.client.post('/code-churn',
-                                    json={"repo_url": "https://github.com/mikaelvesavuori/github-dora-metrics.git",
-                                          "num_commits_before_latest": -1})
-        self.assertEqual(response.status_code, 400)
-        self.assertIn("invalid number of commits", response.get_json().get('error', ''))
+        self.assertEqual(data["added_lines"],5664)
+        self.assertEqual(data["commit_range"],"HEAD~10 to HEAD")
+        self.assertEqual(data["deleted_lines"],819)
+        self.assertEqual(data["method"],"online")
+        self.assertEqual(data["modified_lines"],712)
+        self.assertEqual(data["result"],7195)
+        self.assertEqual(data["total_commits"],297)
 
 if __name__ == '__main__':
     unittest.main()
