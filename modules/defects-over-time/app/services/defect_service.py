79bf1e60516a9485f93be6096a64250df13d1110
Nathaniel Sullivan <nsulliv7@asu.edu>
2025-04-17 15:37:05 -0700

    Add missing imports and update sys.path for module accessibility

--- modules/defects-over-time/app/services/defect_service.py
+++ modules/defects-over-time/app/services/defect_service.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../")))
+
 from datetime import datetime, timedelta
 import requests
 from flask import jsonify, make_response
