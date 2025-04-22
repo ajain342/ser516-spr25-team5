007ecbceccf0b5ef0d8f3b5c9fbbd1adb656e449
Jeteish Pratap Singh <jsing162@asu.edu>
2025-04-21 21:38:12 -0700

    fetch repo mock in mttr test

--- modules/mttr/tests/MTTR_test.py
+++ modules/mttr/tests/MTTR_test.py
@@ -1,6 +1,8 @@
 import unittest
 import os
 import sys
+from unittest.mock import patch
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../")))
 from modules.mttr.main import app
 
@@ -12,7 +14,6 @@ class TestMTTRAPI(unittest.TestCase):
     def test_home_endpoint(self):
         response = self.client.get('/')
         data = response.get_json()
-
         self.assertEqual(response.status_code, 200)
         self.assertIn('message', data)
         self.assertEqual(data['message'], "Visit /mttr to calculate mttr")
@@ -20,52 +21,34 @@ class TestMTTRAPI(unittest.TestCase):
     def test_missing_repo_url(self):
         response = self.client.post('/mttr', json={})
         data = response.get_json()
-
         self.assertEqual(response.status_code, 400)
         self.assertIn('error', data)
         self.assertEqual(data['error'], "Missing repo_url in request")
 
-    # def test_invalid_method_handling(self):
-    #     payload = {
-    #         'repo_url': 'https://github.com/timescale/tsbs',
-    #         'method': 'invalid'
-    #     }
-    #     response = self.client.post('/mttr', json=payload)
-    #     data = response.get_json()
-
-    #     self.assertEqual(response.status_code, 400)
-    #     self.assertIn('error', data)
-    #     self.assertEqual(data['error'], "Invalid method. Use 'online' or 'modified'")
-
-    # def test_missing_method_parameter(self):
-    #     payload = {
-    #         'repo_url': 'https://github.com/timescale/tsbs'
-    #     }
-    #     response = self.client.post('/mttr', json=payload)
-    #     data = response.get_json()
-
-    #     self.assertEqual(response.status_code, 400)
-    #     self.assertIn('error', data)
-    #     self.assertEqual(data['error'], "Invalid method. Use 'online' or 'modified'")
-
-    def test_repository_with_no_issues(self):
+    @patch('modules.mttr.main.fetch_mttr_gitapi')
+    @patch('modules.mttr.main.fetch_repo')
+    def test_repository_with_no_issues(self, mock_fetch_repo, mock_fetch_mttr):
+        mock_fetch_repo.return_value = ("dummysha", "/some/path")
+        mock_fetch_mttr.return_value = {
+            "error": "No closed issues found"
+        }
         payload = {
-            "repo_url": "https://github.com/Siddharthbadal/Python-Projects"
+            "repo_url": "https://github.com/Siddharthbadal/Python-Projects",
+            "method": "modified"
         }
         response = self.client.post('/mttr', json=payload)
         data = response.get_json()
-
         self.assertEqual(response.status_code, 400)
         self.assertIn('error', data)
         self.assertIn('No closed', data['error'])
 
     def test_calculation(self):
         payload = {
-            "repo_url": "https://github.com/timescale/tsbs"
+            "repo_url": "https://github.com/timescale/tsbs",
+            "method": "modified"
         }
         response = self.client.post('/mttr', json=payload)
         data = response.get_json()
-
         if response.status_code == 200:
             self.assertIn('repo_url', data)
             self.assertIn('result', data)
